fa67801ac6dceba5d49743deea684cbd74dd7126
Ubuntu <azureuser@t2vg-a100-G4-49.diqmyit5jjyepaqctehq5sxb2d.ix.internal.cloudapp.net>
2025-03-11 13:44:00 +0000

    modify imagenet.py; add eval.py, sgm/data/coco.py

--- sgm/data/coco.py
+++ sgm/data/coco.py
@@ -1,28 +1,57 @@
+import json
 import os
 from typing import Optional
 
+import numpy as np
 import pytorch_lightning as pl
 from omegaconf import DictConfig
+from PIL import Image
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
-from torchvision.datasets import ImageFolder
 
 
-class ImageNetDataset(Dataset):
+class COCODataset(Dataset):
     def __init__(self, root_dir, split="train", transform=None):
         self.root_dir = root_dir
         self.split = split
         self.transform = transform
-        self.dataset = ImageFolder(os.path.join(root_dir, split), transform=self.transform)
+
+        data_json = os.path.join(root_dir, "annotations", f"captions_{split}2017.json")
+        with open(data_json, "r") as json_file:
+            self.json_data = json.load(json_file)
+            self.img_id_to_filepath = dict()
+            self.img_id_to_captions = dict()
+
+        imagedirs = self.json_data["images"]
+        self.labels = {"image_ids": list()}
+        for imgdir in imagedirs:
+            self.img_id_to_filepath[imgdir["id"]] = os.path.join(
+                root_dir, f"{split}2017", imgdir["file_name"]
+            )
+            self.img_id_to_captions[imgdir["id"]] = list()
+            self.labels["image_ids"].append(imgdir["id"])
+
+        capdirs = self.json_data["annotations"]
+        for capdir in capdirs:
+            # there are in average 5 captions per image
+            self.img_id_to_captions[capdir["image_id"]].append(np.array(capdir["caption"]))
 
     def __len__(self):
-        return len(self.dataset)
+        return len(self.labels["image_ids"])
 
     def __getitem__(self, idx):
-        return {"jpg": self.dataset[idx][0], "cls": self.dataset[idx][1]}
+        img_path = self.img_id_to_filepath[self.labels["image_ids"][idx]]
+        image = Image.open(img_path)
+        if image.mode != "RGB":
+            image = image.convert("RGB")
+        image = self.transform(image)
+        captions = self.img_id_to_captions[self.labels["image_ids"][idx]]
+        # randomly draw one of all available captions per image
+        caption = captions[np.random.randint(0, len(captions))]
+        return {"jpg": image, "cls": [str(caption)]}
 
 
-class ImageNetLoader(pl.LightningDataModule):
+class COCOLoader(pl.LightningDataModule):
     def __init__(
         self,
         batch_size: int,
@@ -55,11 +84,11 @@ class ImageNetLoader(pl.LightningDataModule):
                 transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], inplace=True),
             ])
 
-        self.train_dataset = ImageNetDataset(
+        self.train_dataset = COCODataset(
             root_dir=train.root_dir, split="train", transform=transform
         )
         if validation is not None:
-            self.test_dataset = ImageNetDataset(
+            self.test_dataset = COCODataset(
                 root_dir=validation.root_dir, split="val", transform=transform
             )
         else:
