a59b75037a7c9511cb527873ba208157b5969272
Yuqian Hong <austin712@mail.ustc.edu.cn>
2025-09-09 17:10:45 +0800

    Update coco.py

--- sgm/data/coco.py
+++ sgm/data/coco.py
@@ -78,7 +78,7 @@ class COCOLoader(pl.LightningDataModule):
         if train.get("transform", None):
             size = train.get("size", 256)
             transform = transforms.Compose([
-                transforms.Resize(size, interpolation=Image.BICUBIC),
+                transforms.Resize(size),
                 transforms.CenterCrop(size),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], inplace=True),
